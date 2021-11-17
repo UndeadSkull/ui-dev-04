97097b0e516f770a5177a1c7151af1e9453cf822
Muhammed Favas <favasme08@gmail.com>
2021-11-18 03:01:46 +0530

    Homepage Animations

--- index.js
+++ index.js
@@ -23,3 +23,33 @@ const switchSection = (section) => {
 
     }
 }
+const slideUp = document.querySelectorAll(".slideup");
+const slideIn = document.querySelectorAll(".slidein");
+
+const appearOptions = {
+    // threshold: 1,
+    rootMargin: "-50px 0px 0px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+    entries,
+    appearOnScroll
+) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+},
+    appearOptions);
+
+slideIn.forEach(slideIn => {
+    appearOnScroll.observe(slideIn);
+});
+
+slideUp.forEach(slideUp => {
+    appearOnScroll.observe(slideUp);
+});
